a001c1b37727cd91ff5e1bc3343c2a02ccf895ab
Piyush14Codes <132090283+Piyush14Codes@users.noreply.github.com>
2025-05-02 22:06:34 +0530

    Bengaluru Housing Price Regression Model Pushed to GitHub

---
+++ server/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import util
+import warnings
+warnings.filterwarnings('ignore')
+app = Flask(__name__)
+CORS(app)
+@app.route('/get_location_names')
+def get_location_names():
+    locations = util.get_location_names()
+    print(locations)
+    response = jsonify({
+        'locations': locations
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+@app.route('/predict_home_price', methods = ['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    return response
+if __name__ == "__main__":
+    print("starting python flask server for home price prediction")
+    util.load_saved_artifacts()
+    app.run()
